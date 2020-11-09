4d458afe45340d1158d309b6bbda47e97a38c952
Hiroaki Ikeda <fusianasan.jimso@gmail.com>
2020-11-09 21:49:41 +0900

    7th commit

--- unitdata/unitdata/settings.py
+++ unitdata/unitdata/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
     'app',
     'rest_framework',
     'psycopg2',
-    #'corsheaders',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -35,8 +35,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'corsheaders.middleware.CorsMiddleware',
-    #'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 CORS_ORIGIN_WHITELIST = [
